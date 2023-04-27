338712ad79790ac73d81b18fb9af806615d51a52
leartpro <lenni.protte@gmx.de>
2023-04-27 19:53:26 +0200

    implementation of flanck

--- src/main.cpp
+++ src/main.cpp
@@ -17,18 +17,26 @@ int main() {
                     /* Create a subcommand type option for "dog". */
                     dpp::command_option(dpp::co_sub_command, "interpret_code", "Specifies the Input")
                             .add_option(dpp::command_option(dpp::co_string, "program", "paste your code here", true))
-                            .add_option(dpp::command_option(dpp::co_string, "input_flag", "use -b or -a", false)).set_auto_complete(true)
-                            .add_option(dpp::command_option(dpp::co_string, "output_flag", "use -b or -a", false)).set_auto_complete(true)
-                            .add_option(dpp::command_option(dpp::co_string, "user_input", "give your input here", false))
+                            .add_option(dpp::command_option(dpp::co_string, "input_flag", "use -b or -a",
+                                                            false)).set_auto_complete(true)
+                            .add_option(dpp::command_option(dpp::co_string, "output_flag", "use -b or -a",
+                                                            false)).set_auto_complete(true)
+                            .add_option(
+                                    dpp::command_option(dpp::co_string, "user_input", "give your input here", false))
             );
 
             flanck.add_option(
                     /* Create a subcommand type option for "dog". */
                     dpp::command_option(dpp::co_sub_command, "interpret_file", "Specifies the Input")
-                            .add_option(dpp::command_option(dpp::co_attachment, "program", "attach your .flanck file here", true))
-                            .add_option(dpp::command_option(dpp::co_string, "input_flag", "use -b or -a", false)).set_auto_complete(true)
-                            .add_option(dpp::command_option(dpp::co_string, "output_flag", "use -b or -a", false)).set_auto_complete(true)
-                            .add_option(dpp::command_option(dpp::co_string, "user_input", "give your input here", false))
+                            .add_option(
+                                    dpp::command_option(dpp::co_attachment, "program", "attach your .flanck file here",
+                                                        true))
+                            .add_option(dpp::command_option(dpp::co_string, "input_flag", "use -b or -a",
+                                                            false)).set_auto_complete(true)
+                            .add_option(dpp::command_option(dpp::co_string, "output_flag", "use -b or -a",
+                                                            false)).set_auto_complete(true)
+                            .add_option(
+                                    dpp::command_option(dpp::co_string, "user_input", "give your input here", false))
             );
             /* Create command */
             bot.global_command_create(flanck);
@@ -47,48 +55,72 @@ int main() {
             if (subcommand.name == "interpret_code") {
                 /* Checks if the subcommand has any options. */
                 if (!subcommand.options.empty()) {
-                    /* Get the user from the parameter */
-                    const dpp::user &user = interaction.get_resolved_user(
-                            subcommand.get_value<dpp::snowflake>(0)
-                    );
-                    event.reply(user.get_mention() + " has now been turned into a dog.");
+                    //init arguments
+                    const char* programText = get<string>(subcommand.options[0].value).c_str();
+                    string user_input = get<string>(subcommand.options[3].value);
+                    bool input_as_binary = get<string>(subcommand.options[1].value) == "-b";
+                    bool output_as_binary = get<string>(subcommand.options[2].value) == "-b";
+                    vector<Stack> stacks;
+                    vector<string> input_stacks;
+                    //init user input
+                    std::string delimiter = " ";
+                    size_t position;
+                    std::string token;
+                    while ((position = user_input.find(delimiter)) != std::string::npos) {
+                        token = user_input.substr(0, position);
+                        input_stacks.push_back(token);
+                        user_input.erase(0, position + delimiter.length());
+                    }
+                    input_stacks.push_back(user_input);
+                    //init stacks from user input
+                    stacks.reserve(input_stacks.size());
+                    for (const string& s : input_stacks) {
+                        Stack stack = input_as_binary ?
+                                      Stack::fromBinaryString(s) :
+                                      Stack::fromString(s);
+                        stacks.push_back(stack);
+                    }
+                    //interpret program
+                    Lexer lexer(programText);
+                    Parser parser(lexer);
+                    Interpreter interpreter(parser, stacks);
+                    interpreter.interpret();
+                    //get result
+                    string result = output_as_binary ?
+                            interpreter.getOutputStack().toBinaryString() :
+                            interpreter.getOutputStack().toString();
+                    //return result
+                    event.reply(result);
                 } else {
                     /* Reply if there were no options */
-                    event.reply("No user specified");
+                    event.reply("suspicious usage");
                 }
             }
             /* Check if the subcommand is "cat" */
             if (subcommand.name == "interpret_file") {
                 /* Checks if the subcommand is "interpret_file" */
                 if (!subcommand.options.empty()) {
-                    /* Get the user from the parameter */
-                    const dpp::user &user = interaction.get_resolved_user(
-                            subcommand.get_value<dpp::snowflake>(0)
-                    );
-                    event.reply(user.get_mention() + " has now been turned into a cat.");
+                    event.reply("currently unavailable");
                 } else {
                     /* Reply if there were no options */
-                    event.reply("No user specified");
+                    event.reply("suspicious usage");
                 }
             }
         }
     });
 
-    bot.on_autocomplete([&bot](const dpp::autocomplete_t & event) {
-        for (auto & opt : event.options) {
-            /* The option which has focused set to true is the one the user is typing in */
+    bot.on_autocomplete([&bot](const dpp::autocomplete_t &event) {
+        for (auto &opt: event.options) {
             if (opt.focused) {
-                /* In a real world usage of this function you should return values that loosely match
-                 * opt.value, which contains what the user has typed so far. The opt.value is a variant
-                 * and will contain the type identical to that of the slash command parameter.
-                 * Here we can safely know it is string.
-                 */
-                std::string uservalue = std::get<std::string>(opt.value);
-                bot.interaction_response_create(event.command.id, event.command.token, dpp::interaction_response(dpp::ir_autocomplete_reply)
-                        .add_autocomplete_choice(dpp::command_option_choice("-b", "binary flag"))
-                        .add_autocomplete_choice(dpp::command_option_choice("-a", "ascii flag"))
+                std::string input = std::get<std::string>(opt.value);
+                bot.interaction_response_create(event.command.id, event.command.token,
+                                                dpp::interaction_response(dpp::ir_autocomplete_reply)
+                                                        .add_autocomplete_choice(
+                                                                dpp::command_option_choice("-b", "binary flag"))
+                                                        .add_autocomplete_choice(
+                                                                dpp::command_option_choice("-a", "ascii flag"))
                 );
-                bot.log(dpp::ll_debug, "Autocomplete " + opt.name + " with value '" + uservalue + "' in field " + event.name);
+                //bot.log(dpp::ll_debug, "Autocomplete " + opt.name + " with value '" + input + "' in field " + event.name);
                 break;
             }
         }
